3b0052ea4335c8e5651c16275690b5af86413162
krish <krishnendughosal999@gmail.com>
2024-08-28 17:57:32 +0530

    1905/153

---
+++ arrays/countSubIslands1905.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If we find a land cell in grid2 that hasn't been visited
+                if (grid2[i][j] == 1) {
+                    // Check if this island is a sub-island
+                    if (isSubIsland(grid1, grid2, i, j, m, n)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+
+    private boolean isSubIsland(int[][] grid1, int[][] grid2, int i, int j, int m, int n) {
+        // If out of bounds or the cell is water in grid2, return true (base case)
+        if (i < 0 || i >= m || j < 0 || j >= n || grid2[i][j] == 0) {
+            return true;
+        }
+
+        // If the current cell in grid2 is land, but not in grid1, it's not a sub-island
+        if (grid2[i][j] == 1 && grid1[i][j] == 0) {
+            return false;
+        }
+
+        // Mark the cell as visited in grid2
+        grid2[i][j] = 0;
+
+        // Perform DFS in all four directions
+        boolean top = isSubIsland(grid1, grid2, i - 1, j, m, n);
+        boolean bottom = isSubIsland(grid1, grid2, i + 1, j, m, n);
+        boolean left = isSubIsland(grid1, grid2, i, j - 1, m, n);
+        boolean right = isSubIsland(grid1, grid2, i, j + 1, m, n);
+
+        // The current island is a sub-island only if all directions return true
+        return top && bottom && left && right;
+    }
+}
---
+++ arrays/maxProductSubArray153.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        // Edge case: If the array is empty
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // Initialize variables
+        int maxProductSoFar = nums[0];
+        int minProductSoFar = nums[0];
+        int result = nums[0];
+
+        // Iterate through the array starting from the second element
+        for (int i = 1; i < nums.length; i++) {
+            int currentNum = nums[i];
+
+            // Temporary variable to hold the current max product
+            int tempMax = Math.max(currentNum, Math.max(maxProductSoFar * currentNum, minProductSoFar * currentNum));
+            // Update the min product for current number
+            minProductSoFar = Math.min(currentNum, Math.min(maxProductSoFar * currentNum, minProductSoFar * currentNum));
+
+            // Update the max product
+            maxProductSoFar = tempMax;
+
+            // Update the global result
+            result = Math.max(result, maxProductSoFar);
+        }
+
+        return result;
+    }
+}
