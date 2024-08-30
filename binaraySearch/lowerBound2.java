9ff40ead0fa90109470605b439465180d622fdaf
krish <krishnendughosal999@gmail.com>
2024-08-30 14:29:56 +0530

    sperate binary folder,uperBound,lowerbound,bs-35/ strings -443,

---
+++ binaraySearch/lowerBound2.java
@@ -0,0 +1,29 @@
+
+public class lowerBound2 {
+
+    public static int lowerBound(int []arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};
+        int n = 5, x = 9;
+        int ind = lowerBound(arr, n, x);
+        System.out.println("The lower bound is the index: " + ind);
+    }
+} 
+
---
+++ strings/stringsComparison443.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+        int write = 0;  // Position to write in the chars array
+        int i = 0;  // Position to read in the chars array
+        
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+            
+            // Count the occurrences of the current character
+            while (i < chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+            
+            // Write the character to the array
+            chars[write++] = currentChar;
+            
+            // If count is more than 1, write the digits of the count
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+        
+        return write;
+    }
+}
